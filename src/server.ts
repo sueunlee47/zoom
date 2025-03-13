019a2d866836013b9a555f2d95dccb98527c6486
sueunlee <sue@playauto.co.kr>
2025-03-13 18:03:00 +0900

    feat: 단순 서버 호출 방식 변경

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
-import express, { Express } from "express";
+import express from 'express';
 
-const app: Express = express();
-console.log("hello");
+const app = express();
+const port = 3000;
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
