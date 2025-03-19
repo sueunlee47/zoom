7eb429404598ff874f7c2cfda241e37e6da4e056
sueunlee <sue@playauto.co.kr>
2025-03-19 17:17:44 +0900

    refactor: port 상수, 대문자 처리

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
